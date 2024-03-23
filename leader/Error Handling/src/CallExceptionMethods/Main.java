5f7d9b8e7a9f4c7d7cecac953c168e0e19e73854
Max <2698778050@qq.com>
2024-03-23 13:41:42 +0800

    Exo 2

---
+++ leader/Error Handling/src/CallExceptionMethods/Main.java
@@ -0,0 +1,14 @@
+package CallExceptionMethods;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            System.err.println("Caught Exception");
+            e.getMessage();
+            System.err.println("getLocalizedMessage():" + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+}
