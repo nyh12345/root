6009654c3ec831b0d469ba6eeb9f4037ba33c942
Jeremy <2941652541@qq.com>
2024-03-23 13:22:37 +0800

    Exception specified

--- collab/Error Handling/src/dividedbyzero/Main.java
+++ collab/Error Handling/src/dividedbyzero/Main.java
@@ -1,9 +1,11 @@
-package dividebyzero;
+ppackage dividebyzero;
+
 public class Main {
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println(3/0);
+        try {
+            System.out.println(3/0);
+        } catch (ArithmeticException e) {
+            System.err.println("Caught ArithmeticException: Division by zero.");
+        }
     }
 }
